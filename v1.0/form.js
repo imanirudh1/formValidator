94574244380b7e5b7b1ab9ffbddf7d075438efaa
Anirudh Banerjee <anirudhbanerjee14@gmail.com>
2020-03-03 21:54:17 +0530

    Modified form validator

---
+++ v1.0/form.js
@@ -0,0 +1,51 @@
+const form=document.getElementById('form');
+const username=document.getElementById('username');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
+
+function showError(input,message){
+    const formControl=input.parentElement;
+    formControl.className='form-control error';
+    const small=formControl.querySelector('small');
+    small.innerText=message;
+}
+function showSucess(input){
+    const formControl=input.parentElement;
+    formControl.className='form-control success';
+}
+function isValidEmail(email){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    if(username.value===''){
+        showError(username,'Username is Required')
+    }
+    else{
+        showSucess(username)
+    }
+    if(email.value===''){
+        showError(email,'Email is Required')
+    }
+    else if(!isValidEmail(email.value)){
+        showError(email,'Email is not Valid')
+    }
+    else{
+        showSucess(email)
+    }
+    if(password.value===''){
+        showError(password,'Password is Required')
+    }
+    else{
+        showSucess(password)
+    }
+    if(password2.value===''){
+        showError(password2,'Confirm Password is Required')
+    }
+    else{
+        showSucess(password2)
+    }
+});
